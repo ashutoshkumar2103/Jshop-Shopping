be85bb7c234a0ff5c20a418518691c6f99012b40
Ashutosh Kumar <ashu80934@gmail.com>
2024-10-20 16:09:14 +0530

    Delete Spring_Major_Project/j-shop-spring-boot directory

--- Spring_Major_Project/j-shop-spring-boot/src/main/java/com/jshop/jshopspringboot/repository/ProductOwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jshop.jshopspringboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jshop.jshopspringboot.dto.ProductOwner;
-
-public interface ProductOwnerRepository extends JpaRepository<ProductOwner, Integer> {
-	public ProductOwner findByProductOwnerEmail(String email);
-}
--- Spring_Major_Project/j-shop-spring-boot/src/main/java/com/jshop/jshopspringboot/repository/UserCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jshop.jshopspringboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jshop.jshopspringboot.dto.UserCart;
-
-public interface UserCartRepository extends JpaRepository<UserCart, Integer> {
-
-}
